686c01feb834b5b7330a4174aa32e95befa359e7
Roland Kajatin <roland.kajatin@gmail.com>
2024-08-11 18:26:33 +0200

    Initial Commit

---
+++ Arterio/ArterioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArterioApp.swift
+//  Arterio
+//
+//  Created by Roland Kajatin on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ArterioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
